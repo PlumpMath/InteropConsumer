a8071c661b783309159fdcc314904a4a8141883f
asistente <asistente@157.253.204.70>
2014-04-07 18:29:36 -0500

    Interop Consumers

--- product.service.subsystem/src/main/java/co/edu/uniandes/csw/product/logic/ejb/ProductLogicService.java
+++ product.service.subsystem/src/main/java/co/edu/uniandes/csw/product/logic/ejb/ProductLogicService.java
@@ -11,10 +11,18 @@ import co.edu.uniandes.csw.product.logic.api.IProductLogicService;
 import co.edu.uniandes.csw.product.logic.dto.ProductDTO;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ws.rs.core.MediaType;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.type.TypeFactory;
@@ -22,15 +30,51 @@ import org.codehaus.jackson.map.type.TypeFactory;
 @Default
 @Stateless
 @LocalBean
+
 public class ProductLogicService extends _ProductLogicService implements IProductLogicService {
+
+    public static String URL_SERVICIO = "http://localhost:8080/producto.service.subsystem.web";
 //Se reimplementa el método 'getProduct' para que consulte en el servicio remoto en vez de la persistencia.
+
+    @PostConstruct
+    public void initService() {
+        Properties prop = new Properties();
+        OutputStream output = null;
+        //Lee del archivo de propiedades de glassfish/domains/domain1/config
+        try {
+            File file = new File("services.properties");
+            FileInputStream fileInput = new FileInputStream(file);
+            Properties properties = new Properties();
+            properties.load(fileInput);
+            fileInput.close();
+            URL_SERVICIO = properties.getProperty("url");
+        } catch (Exception e) {
+            //Sin no existe lo crea por defecto
+            try {
+
+                output = new FileOutputStream("services.properties");
+
+                
+                prop.setProperty("url", "http://localhost:8080/producto.service.subsystem.web");
+
+          
+                prop.store(output, null);
+                output.close();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        } 
+    }
+
     public ProductDTO getProduct(Long id) {
         try {
             //Se crea un cliente apache
             Client client = Client.create();
-            
+
             //SE CONSUME EL SERVICIO REMOTO DE PRODUCTOS getProducto
-            WebResource webResource = client.resource("http://localhost:8080/producto.service.subsystem.web/webresources/Producto/" + id);
+            WebResource webResource = client.resource(URL_SERVICIO + "/webresources/Producto/" + id);
             // Se instancia un nuevo object mapper para convertir el string JSON a un DTO
             ObjectMapper map = new ObjectMapper();
             //Se obtiene el string JSON por medio de get
@@ -44,11 +88,12 @@ public class ProductLogicService extends _ProductLogicService implements IProduc
         return null;
     }
 //Se reimplementa el método 'getProducts' de _productLogicService para que consulte la lista de productos en el servicio de la otra aplicación.
+
     public List<ProductDTO> getProducts() {
         try {
             Client client = Client.create();
             //SE CONSUME EL SERVICIO REMOTO DE PRODUCTOS getProductos
-            WebResource webResource = client.resource("http://localhost:8080/producto.service.subsystem.web/webresources/Producto");
+            WebResource webResource = client.resource(URL_SERVICIO + "/webresources/Producto");
             // Se instancia un nuevo object mapper para convertir el string JSON a un DTO
             ObjectMapper map = new ObjectMapper();
             //Se convierte el String a un DTO y luego se retorna
